deb277d3abd1400287a1f415de33644314299a11
philipbeck <phil@newhuaweiapstudent-10-58-28-182.ncl.ac.uk>
2016-05-04 22:14:46 +0100

    script now produces half a clickbait

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
+//all kinds of arrays go here
+var nouns = ["cat gifs", "wedding photos", "dank memes", "legal highs",
+"teens", "life hacks"];
 
 $(document).ready(function(){
   //main method for the button
   $("#generator-button").click(function(){
-    $("#clickbait-title").text("super swagtastic clickbait title shall appear here");
+    $("#clickbait-title").text(generateTitle());
   });
 });
+
+
+function generateTitle(){
+  //variable for random number the number 0 will change as I do more varieties of bait
+  var i = parseInt(Math.random() * 0,10);
+  if(i == 0){
+    return baitList();
+  }
+}
+
+function baitList(){
+  var title = "";
+
+  title = title + parseInt(((Math.random() * 45) + 5) , 10);
+  title = title + " " + nouns[parseInt(Math.random() * (nouns.length), 10)];
+  return title;
+}
