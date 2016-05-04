0109eb6528896bc6e062f9a3791c99309865b02f
philipbeck <phil@newhuaweiapstudent-10-58-28-182.ncl.ac.uk>
2016-05-04 22:27:43 +0100

    adjectives added

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 //all kinds of arrays go here
+//for the subject of most articles
 var nouns = ["cat gifs", "wedding photos", "dank memes", "legal highs",
-"teens", "life hacks"];
+"teens", "life hacks", "cysts being popped"];
+//sometimes articles want more description
+var adjectives = ["cool", "rad", "horrifying", "soothing"];
 
 $(document).ready(function(){
   //main method for the button
@@ -12,16 +15,27 @@ $(document).ready(function(){
 
 function generateTitle(){
   //variable for random number the number 0 will change as I do more varieties of bait
+  //might weight it for certain kinds to be more common in the future
   var i = parseInt(Math.random() * 0,10);
   if(i == 0){
-    return baitList();
+    return listBait();
   }
 }
 
-function baitList(){
+//the standard variety of clickbait
+function listBait(){
   var title = "";
 
   title = title + parseInt(((Math.random() * 45) + 5) , 10);
-  title = title + " " + nouns[parseInt(Math.random() * (nouns.length), 10)];
+  //decide whether to have an adjective as well
+  if(Math.random() < 0.7){
+    title = title + " " + getWord(adjectives);
+  }
+  title = title + " " + getWord(nouns);
   return title;
+}
+
+//gets a String at random out of an array
+function getWord(list){
+  return list[parseInt(Math.random() * (list.length), 10)];
 }
